533a91cf27f4d37d5cfcfcc143b39c5fd62934c9
byeong cheol lim <lim6112j@gmail.com>
2022-11-18 15:32:36 +0900

    grpc server added for android

---
+++ node/server_android.js
@@ -0,0 +1,37 @@
+const PROTO_PATH = __dirname + '/../proto/helloworld.proto';
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keppCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+)
+const protoDescriptior = grpc.loadPackageDefinition(packageDefinition);
+const helloworldService = protoDescriptior.helloworld;
+const sayHello = (call, callback) => {
+  let name = call.request.name
+  const message = `Hello , ${name}`
+  console.log(`name : '${name}'`)
+  callback(null, {message})
+}
+function getGrpcServer() {
+  const grpcServer = new grpc.Server()
+  grpcServer.addService(helloworldService.Greeter.service, {
+    sayHello: sayHello,
+  })
+  return grpcServer
+}
+const grpcServer = getGrpcServer()
+console.log('Starting gRPC server on port 0.0.0.0:50051....')
+grpcServer.bindAsync(
+  '0.0.0.0:50051',
+  grpc.ServerCredentials.createInsecure(), () => {
+    grpcServer.start()
+  }
+)
